33b0acc04ad0780aac377017dedcb87c445052ae
Pedro Antonio de Faria Garcia <66686441+PedroAFG@users.noreply.github.com>
2024-10-21 16:02:35 -0300

    Delete src directory
    reorganizacao

--- src/main/java/com/estoqueplan/estoque_plan/repository/ClienteRepository.java
+++
@@ -1,8 +0,0 @@
-package com.estoqueplan.estoque_plan.repository;
-
-import com.estoqueplan.estoque_plan.model.Cliente;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ClienteRepository extends JpaRepository<Cliente, Long> {
-    
-}
--- src/main/java/com/estoqueplan/estoque_plan/repository/ProdutoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.estoqueplan.estoque_plan.repository;
-
-import com.estoqueplan.estoque_plan.model.Produto;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProdutoRepository extends JpaRepository<Produto, Long> {
-    
-}
-
